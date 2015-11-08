7cb232bad94b9318b3a72fae08740de84e9fc128
Daniel Sokoler <dsokoler@purdue.edu>
2015-11-07 23:59:46 -0500

    Completed Isomorphism generator

--- src/Isomorphism.java
+++ src/Isomorphism.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class Isomorphism {
 	/*
@@ -7,10 +8,60 @@ public class Isomorphism {
 	int[]		isomorphism;
 	
 	/*
+	 * Generate an isomorphism based on a Graph
 	 * Randomly choose new positions for vertices in G
 	 * @param G: graph to generate permutation for
 	 */
 	public Isomorphism(Graph G) {
+		this.isomorphism = new int[G.size];
+		for (int i = 0; i < G.size; i++) {
+			this.isomorphism[i] = i;
+		}
 		
+		Random random = new Random();
+		for (int i = 0; i < G.size; i++) {
+			int permute = random.nextInt(G.size);
+			swap(i, permute);
+		}
+	}
+	
+	/*
+	 * Generate an isomorphism based on an integer size
+	 * Randomly choose new positions for vertices in the range of 0 to size
+	 * @param size: size to generate permutation for
+	 */
+	public Isomorphism(int size) {
+		this.isomorphism = new int[size];
+		for (int i = 0; i < size; i++) {
+			this.isomorphism[i] = i;
+		}
+		
+		Random random = new Random();
+		for (int i = 0; i < size; i++) {
+			int permute = random.nextInt(size);
+			swap(i, permute);
+		}
+	}
+	
+	/*
+	 * Swap two indices of an array
+	 */
+	private boolean swap (int one, int two) {
+		int temp = this.isomorphism[one];
+		this.isomorphism[one] = this.isomorphism[two];
+		this.isomorphism[two] = temp;
+		
+		return true;
+	}
+	
+	/*
+	 * Print the isomorphism: mainly for testing purposes
+	 */
+	public void printIsomorphism() {
+		System.out.printf("Size: %d    ", this.isomorphism.length);
+		for (int i = 0; i < this.isomorphism.length; i++) {
+			System.out.printf("[%d]", this.isomorphism[i]);
+		}
+		System.out.println();
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class Main {
 	
@@ -8,7 +9,7 @@ public class Main {
 		/*
 		 * As seen in written protocol:
 		 * G, G2 are public
-		 * G' is isomorphic to G and subgraph of G2 (is private to prover)
+		 * G' is isomorphic to G and subgraph of G2 (is private to prover, GIVEN)
 		 * Q is isomorphic to G2
 		 * Q' is isomorphic to G' and a subgraph of Q
 		 */
@@ -18,8 +19,29 @@ public class Main {
 		 * As seen in written protocol:
 		 * -Alpha: G2 -> Q
 		 * -Beta:  G' -> Q'
-		 * -Gamma: G  -> G'	(is private to prover)
+		 * -Gamma: G  -> G'	(is private to prover, GIVEN)
 		 */
 		Isomorphism alpha, beta, gamma;
+		
+		/*
+		 * Generate isomorphism Alpha from G2
+		 * Generate isomorphism Beta from G'
+		 * Generate graph Q by applying Alpha to G2
+		 * Generate subgraph Q' by applying Beta to G'
+		 */
+		
+		testIsomorphism();
+	}
+	
+	/*
+	 * Print ten isomorphisms of varying size
+	 */
+	public static void testIsomorphism() {
+		System.out.printf("Testing 10 isomorphisms ranging in size from 10 to 20:\n");
+		for (int i = 0; i < 10; i++) {
+			Random random = new Random();
+			Isomorphism test = new Isomorphism(random.nextInt(20 - 10 + 1) + 10);
+			test.printIsomorphism();
+		}
 	}
 }
